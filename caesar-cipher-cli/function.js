7754c6c12b76982c1ac0e3e652844b2dbc16d950
Olga Bugrova <Lelkabygr@mail.ru>
2020-10-13 12:03:26 +0300

    fix: refactor function shifr

--- caesar-cipher-cli/function.js
+++ caesar-cipher-cli/function.js
@@ -1,51 +1,29 @@
-var through2 = require("through2");
-const terminalArgs = require('./terminal-command');
+const through2 = require("through2");
+const { terminalArgs } = require('./terminal-command');
 
-const decoder = through2(function(chunk, enc, callback) {
-  let shift = +terminalArgs.terminalArgs.shift % 26;
-  const alphabetUpperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  const alphabetLowerCase = 'abcdefghijklmnopqrstuvwxyz';
-  const text = chunk.toString();
+const decoder = through2(function (chunk, enc, callback) {
+    const shift = +terminalArgs.shift % 26;
+    const alphabetUpperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const alphabetLowerCase = 'abcdefghijklmnopqrstuvwxyz';
+    const text = chunk.toString();
 
-  if (terminalArgs.terminalArgs.action === 'decode') {
-    for (let i in text) {
-      if (alphabetUpperCase.indexOf(text[i]) + 1) {
-        const idx = alphabetUpperCase.indexOf(text[i]) - shift;
+    for (let symbol in text) {
+        if (alphabetUpperCase.indexOf(text[symbol]) + 1) {
+            const idx = (terminalArgs.action === 'decode') ? alphabetUpperCase.indexOf(text[symbol]) - shift : alphabetUpperCase.indexOf(text[symbol]) + shift;
+            if (idx >= alphabetUpperCase.length) this.push(alphabetUpperCase.substr(idx - alphabetUpperCase.length, 1));
+            this.push(alphabetUpperCase.substr(idx, 1));
+        } else if (alphabetLowerCase.indexOf(text[symbol]) + 1) {
+            const idx = (terminalArgs.action === 'decode') ? alphabetLowerCase.indexOf(text[symbol]) - shift : alphabetLowerCase.indexOf(text[symbol]) + shift;
 
-        if (idx >= alphabetUpperCase.length) this.push(alphabetUpperCase.substr(idx - alphabetUpperCase.length, 1));
-
-        this.push(alphabetUpperCase.substr(idx, 1));
-      } else if (alphabetLowerCase.indexOf(text[i]) + 1) {
-        const idx = alphabetLowerCase.indexOf(text[i]) - shift;
-
-        if (idx >= alphabetLowerCase.length) this.push(alphabetLowerCase.substr(idx - alphabetLowerCase.length, 1));
-
-        this.push(alphabetLowerCase.substr(idx, 1));
-      } else {
-        this.push(text[i]);
-      }
+            if (idx >= alphabetLowerCase.length) this.push(alphabetLowerCase.substr(idx - alphabetLowerCase.length, 1));
+            this.push(alphabetLowerCase.substr(idx, 1));
+        } else {
+            this.push(text[symbol]);
+        }
     }
-  } else {
-    for (let i in text) {
-      if (alphabetUpperCase.indexOf(text[i]) + 1) {
-        const idx = alphabetUpperCase.indexOf(text[i]) + shift;
-        if (idx >= alphabetUpperCase.length) this.push(alphabetUpperCase.substr(idx - alphabetUpperCase.length, 1));
-
-        this.push(alphabetUpperCase.substr(idx, 1));
-      } else if (alphabetLowerCase.indexOf(text[i]) + 1) {
-        const idx = alphabetLowerCase.indexOf(text[i]) + shift;
-
-        if (idx >= alphabetLowerCase.length) this.push(alphabetLowerCase.substr(idx - alphabetLowerCase.length, 1));
-
-        this.push(alphabetLowerCase.substr(idx, 1));
-      } else {
-        this.push(text[i]);
-      }
-    }
-  }
-  callback()
+    callback()
 });
 
 module.exports = {
-  decoder
+    decoder
 };
